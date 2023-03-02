70a1c16a5db3d2d94f677dc3f2910ecd24bdaa9d
Haripriya2003 <haripriyaammu2001@gmail.com>
2023-03-02 21:23:19 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chatgpt';
+  
+}
+export class cardcompnent
+{
+  title='keralam;'
 }
---
+++ src/assets/data.ts
@@ -0,0 +1,69 @@
+export const data = [
+    {
+        title:"TVM",
+        desc:"Thiruvananthapuram, formerly known as Trivandrum, is the capital of the Indian state of Kerala. It is the most populous city in Kerala with a population of 957,730 as of 2011",
+        site:"https://en.wikipedia.org/wiki/Thiruvananthapuram",
+        image:"https://tse4.mm.bing.net/th?id=OIP.S8i2IogoNUOoGcZUZ7MoygHaEt&pid=Api&P=0"
+        
+    },
+    {
+        title:"Kollam",
+        desc:"Kollam, also known by its former name Quilon, is an ancient seaport and city on the Malabar Coast of India bordering the Laccadive Sea, which is a part of the Arabian Sea. It is 71 km north of the state capital Thiruvananthapuram",
+        site:"https://en.wikipedia.org/wiki/Kollam",
+        image:"https://tse4.mm.bing.net/th?id=OIP.XxPJeBGq0UwRei4sq4nfLQHaEK&pid=Api&P=0"
+    },{
+        title:"Pathanamthitta",
+        desc:"one of the 14 districts in the Indian state of Kerala. The district headquarters is in the town of Pathanamthitta",
+        site:"https://en.wikipedia.org/wiki/Pathanamthitta",
+        image:"https://tse4.mm.bing.net/th?id=OIP.m1rE1-qBmjZO4sKeX_tWMwHaDg&pid=Api&P=0"
+    },{
+        title:"idukki",
+        desc:"Idukki, is one of the 14 districts in the Indian state of Kerala. Idukki district lies amid the Cardamom Hills of Western Ghats in Kerala. Idukki district contains two municipal towns - Kattappana and Thodupuzha.",
+        site:"https://idukki.nic.in/en/",
+        image:"https://cdn.s3waas.gov.in/s3757b505cfd34c64c85ca5b5690ee5293/uploads/2018/03/2018031687.jpg"
+    },{
+        title:"kottayam",
+        desc:"Kottayam is a municipal town in the Indian state of Kerala. Flanked by the Western Ghats on the east and the Vembanad Lake and paddy fields of Kuttanad on the west.",
+        site:"https://en.wikipedia.org/wiki/Kottayam",
+        image:"https://tse4.mm.bing.net/th?id=OIP.O7eeCO15NQmqg3lGYvLtBwHaFj&pid=Api&P=0"
+    },
+    {
+        title:"alappuzha",
+        desc:"Alappuzha, also known by its former name Alleppey, is the administrative headquarters of Alappuzha district in the Indian state of Kerala.",
+        site:"https://en.wikipedia.org/wiki/Alappuzha",
+        image:"https://tse1.mm.bing.net/th?id=OIP.H0v-2-wRVyxXtg0akMOGCwHaE8&pid=Api&P=0"
+        
+    },
+    { title:"ernnakulam",
+      desc:"Ernakulam is the Central Business District of the city of Kochi in Kerala, India and has lent its name to the Ernakulam district.",
+      site:"https://ernakulam.nic.in/",
+      image:"https://tse4.mm.bing.net/th?id=OIP.lr5NzscYroJ5mucwRg2vngHaES&pid=Api&P=0"
+    },
+    {
+       title:"thrissur" ,
+       desc:"Thrissur, formerly Trichur, also known by its historical name Thrissivaperur, is a city and the headquarters of the Thrissur district in Kerala, India",
+       site:"https://en.wikipedia.org/wiki/Thrissur",
+       image:"https://tse3.mm.bing.net/th?id=OIP.v_roasdNCUcPbrNqZ51HIgHaE8&pid=Api&P=0",
+
+    },
+    {
+        title:"palakkad",
+        desc:"Palakkad, formerly known as Palghat, historically known as Palakkattussery is a city and municipality in the Indian state of Kerala. It is the administrative headquarters of the Palakkad District. Wikipedia",
+        site:"https://en.wikipedia.org/wiki/Palakkad",
+        image:"https://tse2.mm.bing.net/th?id=OIP.4aBxInQFKHnwv2to9epWBAHaEo&pid=Api&P=0",
+
+    }, 
+    {
+        title:"kannur",
+        desc:"Kannur, formerly known in English as Cannanore, is a city and a municipal corporation in the state of Kerala",
+        site:"https://en.wikipedia.org/wiki/Kannur",
+         image:"https://tse2.mm.bing.net/th?id=OIP.Vn13hN7is0iMNCuMueXWXgHaFj&pid=Api&P=0",
+    },
+    {
+        title:"wayanad",
+        desc:"Wayanad is a district in the north-east of Indian state Kerala with administrative headquarters at the municipality of Kalpetta. It is the only plateau in Kerala.",
+        site:"https://www.wayanad.com/",
+        image:"https://tse4.mm.bing.net/th?id=OIP.Gb4MaAHZQ3mhNKya63zx5QHaEj&pid=Api&P=0"
+    },
+   
+]
